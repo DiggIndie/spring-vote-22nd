86847c6ef86b4badea996827cb189bacc3e15185
Hoyoung Byun <hybyun0207@gmail.com>
2025-12-17 01:50:24 +0900

    [FEAT] 기본 세팅 (#8)
    * feat : 의존성 구성
    
    * feat : 기본 응답 구조 구현
    
    * feat : security 설정
    
    ---------
    
    Co-authored-by: Yoonji Lee <estelle0329@ewha.ac.kr>

---
+++ src/main/java/com/diggindie/vote/common/config/security/CustomUserDetailService.java
@@ -0,0 +1,33 @@
+package com.diggindie.vote.common.config.security;
+
+import com.diggindie.vote.domain.member.domain.Member;
+import com.diggindie.vote.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public CustomUserDetails loadUserByUsername(String loginId) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByLoginId(loginId)
+                .orElseThrow(() -> new UsernameNotFoundException("Member not found"));
+
+        return new CustomUserDetails(member.getId(), member.getLoginId(), member.getRole());
+    }
+
+
+    public CustomUserDetails loadByExternalId(String externalId) {
+
+        Member member = memberRepository.findByExternalId(externalId)
+                .orElseThrow(() -> new UsernameNotFoundException("Member not found"));
+
+        return new CustomUserDetails(member.getId(), member.getLoginId(), member.getRole());
+    }
+}
---
+++ src/main/java/com/diggindie/vote/domain/member/repository/MemberRepository.java
@@ -0,0 +1,16 @@
+package com.diggindie.vote.domain.member.repository;
+
+import com.diggindie.vote.domain.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByLoginId(String loginId);
+
+    Optional<Member> findByExternalId(String externalId);
+
+}
