711fd0209e4e808347aab00a35c5f302e6bde28a
Yoonji <estelle0329@ewha.ac.kr>
2025-12-18 16:44:56 +0900

    fix: rds 퍼블릭 서브넷으로 변경

--- infra/main.tf
+++ infra/main.tf
@@ -40,7 +40,7 @@ module "rds" {
   project_name         = var.project_name
   environment          = var.environment
   vpc_id               = module.vpc.vpc_id
-  private_subnet_ids   = module.vpc.private_subnet_ids
+  public_subnet_ids   = module.vpc.public_subnet_ids
   db_name              = var.db_name
   db_username          = var.db_username
   db_password          = var.db_password
--- infra/modules/rds/variables.tf
+++ infra/modules/rds/variables.tf
@@ -15,8 +15,8 @@ variable "vpc_id" {
   type        = string
 }
 
-variable "private_subnet_ids" {
-  description = "프라이빗 서브넷 ID 목록"
+variable "public_subnet_ids" {
+  description = "퍼블릭 서브넷 ID 목록"
   type        = list(string)
 }
 
